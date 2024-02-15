10137218bbe658f9d53e1c02ff87cd97c51ab6bd
HypeCrazed <123018649+HypeCrazed@users.noreply.github.com>
2024-02-14 19:30:51 -0500

    Added the option to do another problem.
    Is this update, I added an option to press any key to do another problem, rather than restarting the entire program. I also changed my variables to camel case to make the code easier to read.

--- Program.cs
+++ Program.cs
@@ -1,47 +1,51 @@
-Console.ForegroundColor = ConsoleColor.Blue;
-Console.Title = "Basic Calculator by HypeCrazed";
+Console.Title = "BasicCalculator";
 
-Console.WriteLine("Enter the first number...");
-var FirstNumber = Console.ReadLine();
-
-Console.WriteLine("Enter the second number...");
-var SecondNumber = Console.ReadLine();
-
-Console.WriteLine("What do you want to do? (add, divide, subtract, multiply)");
-var method = Console.ReadLine();
-
-int num1 = int.Parse(FirstNumber);
-int num2 = int.Parse(SecondNumber);
-
-if (method == "add")
+while (true)
 {
-    int result = num1 + num2;
-    Console.WriteLine(result);
-}
+    Console.WriteLine("Enter the first number...");
+    var firstNumber = Console.ReadLine();
 
-if (method == "divide")
-{
-    if (num2 != 0)
+    Console.WriteLine("Enter the second number...");
+    var secondNumber = Console.ReadLine();
+
+    Console.WriteLine("What do you want to do? (add, divide, subtract, multiply)");
+    var method = Console.ReadLine();
+
+    int num1 = int.Parse(firstNumber);
+    int num2 = int.Parse(secondNumber);
+
+    if (method == "add")
     {
-        int result = num1 / num2;
-        Console.WriteLine(result);
+        int result = num1 + num2;
+        Console.WriteLine($"The answer is {result}");
     }
-    else
+
+    if (method == "divide")
     {
-        Console.WriteLine("Can't divide by zero.");
+        if (num2 != 0)
+        {
+            int result = num1 / num2;
+            Console.WriteLine($"The answer is {result}");
+        }
+        else
+        {
+            Console.WriteLine("Can't divide by zero.");
+        }
     }
-}
 
-if (method == "subtract")
-{
-    int result = num1 - num2;
-    Console.WriteLine(result);
-}
+    if (method == "subtract")
+    {
+        int result = num1 - num2;
+        Console.WriteLine($"The answer is {result}");
+    }
 
-if (method == "multiply")
-{
-    int result = num1 * num2;
-    Console.WriteLine(result);
-}
+    if (method == "multiply")
+    {
+        int result = num1 * num2;
+        Console.WriteLine($"The answer is {result}");
+    }
 
-Console.ReadKey();
+    Console.Write("\nPress any key to do another problem.");
+    Console.ReadKey();
+    Console.Clear();
+}
